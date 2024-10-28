f6501c9cacf3ebd7b08851267599eb968b38b783
yuwuweichun <yuwuweichun522@gmail.com>
2024-10-28 19:53:29 +0800

    forge.config.cjs

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,7 +1,5 @@
-// const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-// const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
