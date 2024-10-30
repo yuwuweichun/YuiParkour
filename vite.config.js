dfb7f86a0ffc7073070e5a7c5d032fe953f540af
yuwuweichun <yuwuweichun522@gmail.com>
2024-10-30 19:49:27 +0800

    base build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
+const base = process.env.BASE || '/';
+const outDir = process.env.OUT_DIR || 'dist';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: base,
+  build: {
+    outDir: outDir
+  },
   plugins: [vue()],
   resolve: {
     alias: {
